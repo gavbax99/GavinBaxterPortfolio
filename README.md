92a97f50da13d8c6107fe4367ab9a2cb3386a823
gavbax99 <gav.bax@gmail.com>
2019-06-07 08:58:52 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# 2019_portfolio
-2019 person portfolio
+# readme
---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    // var a = $(".hey").outerWidth(true);
+    // $(".hey").text(a);
+
+});
+
+// Preloader fade
+$(window).on("load", function() {
+
+});
