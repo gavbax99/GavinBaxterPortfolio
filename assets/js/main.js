f4491ecdd0a7f5cae952ca992dfe19e81fb4c66a
Gavin <gav.bax@gmail.com>
2019-07-23 17:36:55 -0700

    complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,22 @@ $(document).ready(function(){
 
     $(".nav-button").on("click", function () {
         var data = $(this).attr("data-name");
+        var hamburgerMenu = $(".hamburger-list").css("right");
         $('html,body').animate({ scrollTop: $(`#${data}`).offset().top}, 1000);
+        if (hamburgerMenu == "0px") {
+            $(".hamburger-list").css("right", "-100px");
+        };
+    });
+
+    $(".hamburger").on("click", function () {
+        var hamburgerMenu = $(".hamburger-list").css("right");
+        if (hamburgerMenu == "-100px") {
+            $(".hamburger-list").css("right", "0px");
+        };
+    });
+
+    $(".hamburger-x").on("click", function (){
+        $(".hamburger-list").css("right", "-100px");
     });
 
 });
